04e503e63f6f6ddd9e1f0d5f28f2dbb380f0e678
Eric Wang <ywan0375@uni.sydney.edu.au>
2023-10-29 15:57:05 +1100

    1

--- sketch.js
+++ sketch.js
@@ -1,5 +1,33 @@
+let canvasWidth = 1000;
+let canvasHeight = 1000;
+let yellowRects = [];
+
+class yellowRect{
+  constructor(x,y,width,height,rotation){
+    this.x = x ?? 0;
+    this.y = y ?? 0;
+    this.width = width ?? canvasWidth;
+    this.height = height ?? canvasHeight;
+    this.rotation = rotation ?? 0;
+  }
+
+  draw(){
+    push();
+    translate(this.x,this.y);
+    rotate(this.rotation);
+
+    noStroke();
+    fill(255,240,1);
+    rect(0,0,this.width,this.height);
+
+    pop();
+  }
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(canvasWidth, canvasHeight);
+
+  yellowRects.push(new yellowRect(0,20,1000,20));
 }
 
 function draw() {
